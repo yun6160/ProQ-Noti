5c24100392ec8a69aa0de9ed5c47a0006a8995f3
aquaman122 <swj981128@gmail.com>
2025-02-13 18:05:38 +0900

    chore: Eslint 초기 설정

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,8 @@
 import type { NextConfig } from "next";
 
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
 });
 
 const nextConfig: NextConfig = {
