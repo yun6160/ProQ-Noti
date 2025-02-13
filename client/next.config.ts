e01e4352101761f70b390c8a9fe87353ba8ca7be
aquaman122 <swj981128@gmail.com>
2025-02-13 17:02:24 +0900

    feat: next-pwa 빌드

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { NextConfig } from "next";
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
