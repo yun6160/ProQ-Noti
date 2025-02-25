11c869bf6a03a03e89b42376c4fd213c0e1a38bf
yun6160 <devyun6160@gmail.com>
2025-02-26 01:25:10 +0900

    design:add favicon

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,18 +1,23 @@
+import { Metadata } from 'next';
 import './globals.css';
 import Providers from './provider';
+
+export const metadata: Metadata = {
+  title: '프로들의 솔랭을 실시간으로',
+  icons: {
+    icon: '/favicon.ico'
+  }
+};
 
 export default async function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en">
       <body>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
