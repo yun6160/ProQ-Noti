44b45404b15068c734e3c247b8cb099e921bac3e
aquaman122 <swj981128@gmail.com>
2025-03-22 20:02:00 +0900

    feat: redux 초기 세팅 추가

--- client/src/app/provider.tsx
+++ client/src/app/provider.tsx
@@ -1,5 +1,7 @@
 'use client';
+import store from '@/store/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux'
 import { PropsWithChildren, useState } from 'react';
 
 const Providers = ({ children }: PropsWithChildren) => {
@@ -14,7 +16,13 @@ const Providers = ({ children }: PropsWithChildren) => {
     })
   );
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={client}>
+        {children}
+      </QueryClientProvider>
+    </Provider>
+  );
 };
 
 export default Providers;
---
+++ client/src/types/user.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  id: string,
+  email: string | null,
+  user_name: string,
+  avatar_url: string | null,
+  created_at: string,
+}
