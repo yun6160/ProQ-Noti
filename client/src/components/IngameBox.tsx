8eb91ed904f0c921946ac77bd95862cb0914ec78
aquaman122 <swj981128@gmail.com>
2025-04-22 17:10:56 +0900

    feat: 이미지 파일 추가

--- client/src/components/IngameBox.tsx
+++ client/src/components/IngameBox.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, MouseEvent } from 'react';
+import { useState, MouseEvent, useEffect } from 'react';
 import LiveIcon from '@/app/assets/icons/live.svg';
 import { FaRegHeart } from 'react-icons/fa';
 import { FaHeart } from 'react-icons/fa';
@@ -9,6 +9,9 @@ import type { IIngameBoxProps } from '@/types';
 import { useToast } from '@/hooks/use-toast';
 import { useUserId } from '@/utils/hooks/userAuth';
 import { POST } from '@/app/api/subscribe/route';
+import ChampionImage from './ChampionImage';
+import SpellImages from './SpellImages';
+import RuneImages from './RuneImage';
 
 export default function IngameBox({
   pro_name,
@@ -25,8 +28,31 @@ export default function IngameBox({
   const [isSubscribe, setIsSubscribe] = useState<boolean>(
     initialIsSubscribe ?? false
   );
+  const [liveGame, setLiveGame] = useState<any>(null);
+
   const { toast } = useToast();
+  const version = process.env.NEXT_PUBLIC_LEAGUE_PATCH || '15.7.1';
   const userId = useUserId();
+
+  useEffect(() => {
+    if (!puuid || !isOpen) return;
+    fetch(`/api/live-game?summonerId=${puuid}`, { cache: "no-store" })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.inGame) {
+          setLiveGame(json.game);
+        } else {
+          setLiveGame(null);
+        }
+      });
+  }, [puuid, isOpen]);
+
+  const player = liveGame?.participants.find((p: any) => p.puuid === puuid);
+  const spellNames = player ? [player.spell1Name, player.spell2Name] : [];
+  const runePaths =
+  player?.perks?.styles?.[0]?.selections
+    ? player.perks.styles[0].selections.slice(0, 2).map((s: any) => s.icon)
+    : [];
 
   const handleSubscribeClick = async (event: MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
@@ -60,46 +86,23 @@ export default function IngameBox({
           )}
         </button>
       </div>
-      {isOpen && (
+      {isOpen && player && (
         <div className="flex flex-col gap-1 items-center justify-center px-7 py-3 w-[20.69rem] web:w-[30rem] h-[9.25rem] rounded-[10px] shadow-bottom bg-primary-white animate-slideindown">
-          {/* 챔피언, 스펠 2개, 룬 2개 */}
-          <div className="flex gap-2 w-full h-full overflow-hidden items-center justify-center ">
-            <img
-              className="h-full object-contain"
-              src="https://ddragon.leagueoflegends.com/cdn/15.4.1/img/champion/Aatrox.png"
-            ></img>
-            <div className="overflow-hidden h-full flex flex-col gap-1">
-              <img
-                className="object-contain h-1/2"
-                src="https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerFlash.png"
-              ></img>
-              <img
-                className="object-contain h-1/2"
-                src="https://ddragon.leagueoflegends.com/cdn/15.4.1/img/spell/SummonerFlash.png"
-              ></img>
-            </div>
-            <div className="overflow-hidden h-full flex flex-col gap-1">
-              <img
-                className="object-contain h-1/2"
-                src="https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/Domination/Electrocute/Electrocute.png"
-              ></img>
-              <img
-                className="object-contain h-1/2"
-                src="https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/Domination/Electrocute/Electrocute.png"
-              ></img>
-            </div>
+          <div className="flex gap-2 w-full h-full overflow-hidden items-center justify-center">
+            <ChampionImage championName={player.championName} version={version} />
+            <SpellImages spellNames={spellNames} version={version} />
+            <RuneImages runePaths={runePaths} />
           </div>
           <div className="text-body2Bold">
             {summoner_name}
             {tag_line && `#${tag_line}`}
           </div>
-          {/* 녹화랑 큐 타입 */}
           <div className="flex justify-between w-full text-body2">
             <div className="flex gap-1 items-center">
               <FaHourglassStart className="text-primary-mint" />
-              시간
+              {(liveGame?.gameLength / 60).toFixed(1)}분
             </div>
-            <div>큐 타입</div>
+            <div>큐 타입 {liveGame?.gameQueueConfigId}</div>
           </div>
         </div>
       )}
