7d3718825330aff87271c64a3c0dd3875db5e347
aquaman122 <swj981128@gmail.com>
2025-04-30 11:10:52 +0900

    feat: ingameBox 최종코드 수정, 기능 구현

--- client/src/components/RuneImage.tsx
+++ client/src/components/RuneImage.tsx
@@ -1,47 +1,30 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { getRunePath } from "@/utils/hooks/lol";
 
 interface RuneImagesProps {
   runePaths: number[];
 }
 
 export default function RuneImages({ runePaths }: RuneImagesProps) {
-  const [perkData, setPerkData] = useState<Record<number, string>>({});
-
-  useEffect(() => {
-    fetch('/data/perks.json')
-      .then((res) => res.json())
-      .then((data) => {
-        const perkMap: Record<number, string> = {};
-        data.forEach((perk: any) => {
-          perkMap[perk.id] = perk.iconPath;
-        });
-        setPerkData(perkMap);
-      })
-      .catch((err) => console.error('Failed to fetch perks.json', err));
-  }, []);
-
-  const getPrimaryImageSrc = (perkId: number) => `/perks/${perkId}.png`;
-  const getFallbackImageSrc = (perkId: number) => 
-    perkData[perkId]
-      ? `https://raw.communitydragon.org/latest${perkData[perkId]}`
-      : '/perks/default.png';
 
   return (
     <div className="overflow-hidden h-full flex flex-col gap-1">
-      {runePaths.map((perkId, i) => (
-        <img
-          key={i}
-          className="object-contain h-1/2"
-          src={getPrimaryImageSrc(perkId)}
-          onError={(e) => {
-            const fallbackSrc = getFallbackImageSrc(perkId);
-            (e.target as HTMLImageElement).src = fallbackSrc;
-          }}
-          alt={`rune-${i}`}
-        />
-      ))}
+      {runePaths.map((perkId, i) => {
+        
+        return (
+          <img
+            key={i}
+            className="object-contain h-1/2"
+            src={`https://ddragon.leagueoflegends.com/cdn/img/perk-images/${getRunePath(perkId)}.png`}
+            onError={(e) => {
+              e.currentTarget.onerror = null;
+              e.currentTarget.src = `/perks/${perkId}.png`;
+            }}
+            alt={`rune-${perkId}`}
+          />
+        );
+      })}
     </div>
   )
 }
