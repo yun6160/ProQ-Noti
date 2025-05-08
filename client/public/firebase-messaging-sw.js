8efec66930af5b7169aaf8ab030b201b69751917
yun6160 <devyun6160@gmail.com>
2025-05-09 02:40:25 +0900

    feat: fcm-setup

---
+++ client/public/firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+importScripts(
+  'https://www.gstatic.com/firebasejs/11.6.1/firebase-app-compat.js'
+);
+importScripts(
+  'https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging-compat.js'
+);
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyA_E5OoRqxBvnhJOr8SaUEl5Cq_RRZdxI8',
+  authDomain: 'proq-noti.firebaseapp.com',
+  projectId: 'proq-noti',
+  storageBucket: 'proq-noti.firebasestorage.app',
+  messagingSenderId: '286950705449',
+  appId: '1:286950705449:web:25f99cd4c33fa248a8cf38',
+  measurementId: 'G-JT3H0L9YT1'
+});
+
+const messaging = firebase.messaging();
+onBackgroundMessage(messaging, (payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
